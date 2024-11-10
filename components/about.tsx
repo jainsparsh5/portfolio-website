e8709ced542f6f48f3d87008597ba6da89e39df8
SparshJain <jainsparsh17@gmail.com>
2024-11-10 16:16:54 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,34 +19,33 @@ export default function About() {
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
-        Final year student pursuing Bachelor of Technology degree in{" "}
-        <span className="font-medium">Computer Science and Engineering</span> at
-        IIIT Dharwad. I am skilled in{" "}
-        <span className="font-medium">
-          full-stack web development and problem solving
-        </span>
-        . <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          C++, JavaScript, React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with Recoil, postgreSQL, TypeScript and Prisma. I
-        am always looking to learn new technologies. I am currently looking for
-        a <span className="font-medium">full-time position</span> as a software
-        developer/engineer.
+      Associate Software Engineer at Urbint with a Bachelor of Technology degree in{" "}
+      <span className="font-medium">Computer Science and Engineering</span> from
+      IIIT Dharwad. I am skilled in{" "}
+      <span className="font-medium">
+        full-stack web development and problem solving
+      </span>
+      . <span className="italic">My favorite part of programming</span> is the
+      problem-solving aspect. I <span className="underline">love</span> the
+      feeling of finally figuring out a solution to a problem. My core stack
+      is{" "}
+      <span className="font-medium">
+        C++, JavaScript, React, Next.js, Node.js, and MongoDB
+      </span>
+      . I am also familiar with Recoil, postgreSQL, TypeScript and Prisma. 
+      Currently exploring <span className="font-medium">Web3</span> technologies. 
+      I am always looking to learn new technologies and enhance my skills in 
+      software development.
       </p>
-
       <p>
-        <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and go to the gym. I also enjoy{" "}
-        <span className="font-medium">trading and investing</span>. I currently
-        trade in <span className="font-medium"> Futures and Options</span> and I
-        am heavily invested in{" "}
-        <span className="font-medium">Cryptocurrencies</span>. I&apos;m also
-        learning how to play the guitar.
-      </p>
+      <span className="italic">When I&apos;m not coding</span>, I enjoy
+      playing video games, watching movies, and go to the gym. I also enjoy{" "}
+      <span className="font-medium">trading and investing</span>. I currently
+      trade in <span className="font-medium">Futures and Options</span> and I
+      am heavily invested in{" "}
+      <span className="font-medium">Cryptocurrencies</span>. I also play the 
+      guitar.
+    </p>
     </motion.section>
   );
 }
