15085d6427133c4a2a4dbc162c783cae178b9541
Sparsh Jain <jainsparsh17@gmail.com>
2024-11-10 13:59:53 +0530

    Finished pending Form Submission using emailjs

--- actions/sendEmail.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export const sendEmail = async (formData: FormData) => {
-
-};
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,14 +1,36 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
+import { FaPaperPlane, FaRegSmileBeam } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const { ref } = useSectionInView("Contact");
+
+  const form = useRef<any>();
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_1eza72y", "template_rxyqqci", form.current, {
+        publicKey: "IVlDdqGGBNvUF3b6N",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          form.current.reset();
+          setIsSubmitted(true);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   return (
     <motion.section
@@ -34,15 +56,15 @@ export default function Contact() {
       </p>
       <form
         className="mt-10 flex flex-col items-center"
-        action={async (formData) => {
-          await sendEmail(formData);
-        }}
+        ref={form}
+        onSubmit={sendEmail}
       >
         <input
           className="h-14 w-full rounded-lg border border-black/10 px-4"
           placeholder="Your email"
-          name="senderEmail"
+          name="user_email"
           type="email"
+          disabled={isSubmitted}
           required
           maxLength={500}
         />
@@ -50,15 +72,24 @@ export default function Contact() {
           className="h-52 w-full my-3 rounded-lg border border-black/10 p-4"
           placeholder="Your message"
           name="message"
+          disabled={isSubmitted}
           required
           maxLength={1000}
         />
         <button
           type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+          value="Send"
+          disabled={isSubmitted}
+          className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 ${
+            isSubmitted
+              ? "bg-slate-400 hover:bg-slate-400"
+              : "bg-gray-800 hover:bg-gray-950"
+          }`}
         >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+          {isSubmitted ? 
+            <>Sent <FaRegSmileBeam className="text-l opacity-70 transition-all group-hover:scale-100" /></>
+           : <>Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" /></>
+          }
         </button>
       </form>
     </motion.section>
